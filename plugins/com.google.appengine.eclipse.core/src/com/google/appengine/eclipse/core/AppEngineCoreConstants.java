55366540baab02d5bd3b8d0a1046393d1221c910
tparker <tparker@google.com>
2015-01-21 09:44:06 -0800

    Add missing or fix incomplete copyright headers, using the checked-in dates from Piper to set the copyright year.
    After this change and cl/84056515 and cl/84056681 all of the files
    appear to have the correct copyright:
    $ find . -type f -name "*.java" | wc -l
    1167
    $ find . -type f -name "*.java" | xargs grep -l " \* http://www.eclipse.org/legal/epl-v10.html" | wc -l
    1166
    
    With the only missing file being a code template:
    com.google.gdt.eclipse.mobile.android/templates/android/MainActivity.java
    -------------
    Created by MOE: http://code.google.com/p/moe-java
    MOE_MIGRATED_REVID=84442868

--- plugins/com.google.appengine.eclipse.core/src/com/google/appengine/eclipse/core/AppEngineCoreConstants.java
+++ plugins/com.google.appengine.eclipse.core/src/com/google/appengine/eclipse/core/AppEngineCoreConstants.java
@@ -1,3 +1,17 @@
+/*******************************************************************************
+ * Copyright 2014 Google Inc. All Rights Reserved.
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License v1.0 which
+ * accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *******************************************************************************/
 package com.google.appengine.eclipse.core;
 
 /**
@@ -8,7 +22,7 @@ public class AppEngineCoreConstants {
   // TODO(nhcohen): merge constants from com.google.appengine.eclipse.wtp.facet.IGaeFacetConstants
   // and com.google.appengine.eclipse.wtp.maven.Constants into here, as appropriate, and update
   // references accordingly.
-  
+
   public static final String GAE_WAR_FACET_ID = "com.google.appengine.facet";
   public static final String GAE_EAR_FACET_ID = "com.google.appengine.facet.ear";
 }
