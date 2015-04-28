ad2bdc49effd47c4f3c90cf6f1450ebee85ec226
branflake2267@gmail.com <branflake2267@gmail.com>
2015-04-27 17:46:05 -0700

    Updateing release .sh files with cd to repository and notes.

--- repo/upload-site-root.sh
+++ repo/upload-site-root.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 # upload release repo
 
-cd repository
+# install utils
+# https://cloud.google.com/storage/docs/gsutil_install
+
+# Note: allow the public to read the bucket
+# gsutil defacl set public-read gs://gwt-plugin-snapshot
+
+cd repo/target/repository
 
 # upload snapshot
 # http://storage.googleapis.com/gwt-eclipse-plugin
--- repo/upload-snapshot.sh
+++ repo/upload-snapshot.sh
@@ -7,7 +7,8 @@
 # Note: allow the public to read the bucket
 # gsutil defacl set public-read gs://gwt-plugin-snapshot
 
-cd repository
+cd repo/target/repository
 
 # upload snapshot
+# url locaiton: http://storage.googleapis.com/gwt-eclipse-plugin/snapshot
 gsutil cp -r . gs://gwt-eclipse-plugin/snapshot
