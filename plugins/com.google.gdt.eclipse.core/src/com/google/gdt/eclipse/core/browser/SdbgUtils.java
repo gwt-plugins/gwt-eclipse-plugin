c2b0caed4330448c320868ef7a23f56ec64cf036
branflake2267@gmail.com <branflake2267@gmail.com>
2015-05-04 17:47:02 -0700

    Adding sdbg p2 repo to repositories list.

--- plugins/com.google.gdt.eclipse.core/src/com/google/gdt/eclipse/core/browser/SdbgUtils.java
+++ plugins/com.google.gdt.eclipse.core/src/com/google/gdt/eclipse/core/browser/SdbgUtils.java
@@ -5,7 +5,7 @@ package com.google.gdt.eclipse.core.browser;
 
 import com.google.gdt.eclipse.core.CorePluginLog;
 
-import com.github.sdbg.ui.ISDBGLauncher;
+import com.github.sdbg.ui.launchers.IChromeLaunchShortcutExt;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
@@ -15,20 +15,18 @@ import org.eclipse.core.runtime.Platform;
 
 public class SdbgUtils {
 
-  public static String PLUGIN_ID = "com.github.sdbg.debug.ui.launcher";
-
   public static boolean hasSdbgInstalled() {
-    IExtensionPoint extensionPoint = Platform.getExtensionRegistry().getExtensionPoint(ISDBGLauncher.EXTENSION_ID);
+    IExtensionPoint extensionPoint = Platform.getExtensionRegistry().getExtensionPoint(IChromeLaunchShortcutExt.EXTENSION_ID);
     return extensionPoint != null;
   }
 
   public static void openChrome(IResource resource, String mode , String url) {
-    IExtensionPoint extensionPoint = Platform.getExtensionRegistry().getExtensionPoint(ISDBGLauncher.EXTENSION_ID);
+    IExtensionPoint extensionPoint = Platform.getExtensionRegistry().getExtensionPoint(IChromeLaunchShortcutExt.EXTENSION_ID);
     IConfigurationElement[] elements = extensionPoint.getConfigurationElements();
 
     try {
-      ISDBGLauncher ilauncher = (ISDBGLauncher) elements[0].createExecutableExtension("class");
-      ilauncher.launchChrome(resource, mode, url);
+      IChromeLaunchShortcutExt ilauncher = (IChromeLaunchShortcutExt) elements[0].createExecutableExtension("class");
+      ilauncher.launch(resource, mode, url);
     } catch (CoreException e) {
       e.printStackTrace();
       CorePluginLog.logError("SdbgUtils could not launch SDBG. url=" + url, e);
