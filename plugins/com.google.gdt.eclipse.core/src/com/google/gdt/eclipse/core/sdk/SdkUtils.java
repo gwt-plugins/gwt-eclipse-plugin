84cf0ec0f3aad5ecfa42e9679e7043c68cc6a402
branflake2267@gmail.com <branflake2267@gmail.com>
2016-08-10 06:59:16 -0700

    Issue 138 - Add beta to the version parser.

--- plugins/com.google.gdt.eclipse.core/src/com/google/gdt/eclipse/core/sdk/SdkUtils.java
+++ plugins/com.google.gdt.eclipse.core/src/com/google/gdt/eclipse/core/sdk/SdkUtils.java
@@ -58,9 +58,8 @@ public class SdkUtils {
                   currentMaxVersion = version;
                 }
               } catch (NumberFormatException e) {
-                CorePluginLog.logError(e, "Could not compare '"
-                    + currentMaxVersion + "' to '" + version
-                    + "', ignoring this version");
+                CorePluginLog.logError(e,
+                    "Could not compare '" + currentMaxVersion + "' to '" + version + "', ignoring this version");
               }
             }
 
@@ -79,20 +78,17 @@ public class SdkUtils {
     }
 
     /**
-     * Returns the {@link Sdk} that is bound to the {@link IJavaProject} or
-     * <code>null</code> if none.
+     * Returns the {@link Sdk} that is bound to the {@link IJavaProject} or <code>null</code> if none.
      */
-    public abstract Sdk doFindSdk(IJavaProject project)
-        throws JavaModelException;
+    public abstract Sdk doFindSdk(IJavaProject project) throws JavaModelException;
   }
 
   public static final String INTERNAL_VERSION_PREFIX = "0.0.";
   public static final String INTERNAL_VERSION_SUFFIX = "999";
 
   /**
-   * In some cases, the 3rd component of the version string can contain both
-   * digits and non-digits (e.g., 2.1.0M1, 2.1.0-m1); this is incompatible with
-   * what GPE expects for a SDK version string.
+   * In some cases, the 3rd component of the version string can contain both digits and non-digits (e.g., 2.1.0M1,
+   * 2.1.0-m1); this is incompatible with what GPE expects for a SDK version string.
    */
   public static String cleanupVersion(String versionStr) {
     if (StringUtilities.isEmpty(versionStr)) {
@@ -100,7 +96,7 @@ public class SdkUtils {
     }
 
     versionStr = toOSGICompatibleVersionString(versionStr);
-    
+
     String[] versionComponents = versionStr.split("\\.");
 
     if (versionComponents.length != 3) {
@@ -128,27 +124,25 @@ public class SdkUtils {
     }
 
     // Add 4th component
-    return versionComponents[0] + '.' + versionComponents[1] + '.'
-        + lastComponent.substring(0, nonDigitIndex) + '.' + lastComponent.substring(nonDigitIndex);
+    return versionComponents[0] + '.' + versionComponents[1] + '.' + lastComponent.substring(0, nonDigitIndex) + '.'
+        + lastComponent.substring(nonDigitIndex);
   }
 
   /**
-   * Compares two version strings for order. Returns a negative integer, zero,
-   * or a positive integer if the first argument is less than, equal to, or
-   * greater than the second.
+   * Compares two version strings for order. Returns a negative integer, zero, or a positive integer if the first
+   * argument is less than, equal to, or greater than the second.
    * <p>
-   * This method handles version strings that include RC or MS, treating them as
-   * MS < RC < final version.
+   * This method handles version strings that include RC or MS, treating them as MS < RC < final version.
    *
    * @param version1
    * @param version2
-   * @return a negative integer, zero, or a positive integer if the first
-   *         argument is less than, equal to, or greater than the second
+   * @return a negative integer, zero, or a positive integer if the first argument is less than, equal to, or greater
+   *         than the second
    * @throws NumberFormatException
-   * @throws NullPointerException if either version string is null
+   * @throws NullPointerException
+   *           if either version string is null
    */
-  public static int compareVersionStrings(String version1, String version2)
-      throws NumberFormatException {
+  public static int compareVersionStrings(String version1, String version2) throws NumberFormatException {
     version1 = SdkUtils.cleanupVersion(version1);
     version2 = SdkUtils.cleanupVersion(version2);
     String splitRegex = "[._-]";
@@ -167,11 +161,10 @@ public class SdkUtils {
   }
 
   /**
-   * Returns an {@link Sdk} with a matching installation path from the
-   * {@link SdkSet} or <code>null</code> if none could be found.
+   * Returns an {@link Sdk} with a matching installation path from the {@link SdkSet} or <code>null</code> if none could
+   * be found.
    */
-  public static <T extends Sdk> T findSdkForInstallationPath(
-      SdkSet<T> sdkSet, IPath installationPath) {
+  public static <T extends Sdk> T findSdkForInstallationPath(SdkSet<T> sdkSet, IPath installationPath) {
     for (T sdk : sdkSet) {
       if (JavaUtilities.equalsWithNullCheck(sdk.getInstallationPath(), installationPath)) {
         return sdk;
@@ -182,14 +175,14 @@ public class SdkUtils {
   }
 
   /**
-   * Generates a name that is unique to a given {@link SdkSet} based on the name
-   * prefix.
+   * Generates a name that is unique to a given {@link SdkSet} based on the name prefix.
    *
-   * @param namePrefix name prefix for the generated name
-   * @param sdks {@link SdkSet} for within which a unique name is needed
+   * @param namePrefix
+   *          name prefix for the generated name
+   * @param sdks
+   *          {@link SdkSet} for within which a unique name is needed
    *
-   * @return name that is unique to a given {@link SdkSet} based on the name
-   *         prefix
+   * @return name that is unique to a given {@link SdkSet} based on the name prefix
    */
   public static String generateUniqueSdkNameFrom(String namePrefix, SdkSet<? extends Sdk> sdks) {
     String generatedName = namePrefix;
@@ -214,18 +207,15 @@ public class SdkUtils {
   }
 
   /**
-   * Returns a negative numeric representation of the prerelease version string.
-   * This is negative so all prereleases are less than the corresponding
-   * release.
+   * Returns a negative numeric representation of the prerelease version string. This is negative so all prereleases are
+   * less than the corresponding release.
    *
-   * @param versionString must start with either "rc" or "ms" followed by a
-   *        number that is less than 1000
-   * @return a negative numeric representation such that r(rc1) < r(rc10) and
-   *         r(ms__) < r(rc1)
+   * @param versionString
+   *          must start with either "rc", "beta" or "ms" followed by a number that is less than 1000
+   * @return a negative numeric representation such that r(rc1) < r(rc10) and r(ms__) < r(rc1)
    * @throws NumberFormatException
    */
-  private static int parsePrereleaseVersionString(String versionString)
-      throws NumberFormatException {
+  private static int parsePrereleaseVersionString(String versionString) throws NumberFormatException {
     String prereleaseType = versionString.substring(0, 2);
     int version = Integer.parseInt(versionString.substring(2));
     // Since it must be negative, (1000 - version) so rc1 < rc10
@@ -244,7 +234,7 @@ public class SdkUtils {
     if (componentIndex < versionComponents.length) {
       String component = versionComponents[componentIndex].trim().toLowerCase();
       if (component.length() > 0) {
-        if (component.startsWith("rc") || component.startsWith("ms")) {
+        if (component.startsWith("rc") || component.startsWith("ms") || component.startsWith("beta")) {
           return parsePrereleaseVersionString(component);
         } else {
           return Integer.parseInt(component);
@@ -256,20 +246,18 @@ public class SdkUtils {
   }
 
   /**
-   * Return a string where the modifier is separated from the version number by
-   * a '.' instead of a '-'. OSGI strings want a '.' to separate the modifier
-   * from the rest of the version number.
+   * Return a string where the modifier is separated from the version number by a '.' instead of a '-'. OSGI strings
+   * want a '.' to separate the modifier from the rest of the version number.
    */
   private static String toOSGICompatibleVersionString(String versionString) {
     /*
-     * Note this method does try to parse out the version number, to ensure that
-     * the replacement only takes place at the start of the qualifier.
+     * Note this method does try to parse out the version number, to ensure that the replacement only takes place at the
+     * start of the qualifier.
      */
     return versionString.replaceFirst("-", ".");
   }
 
-
-
   private SdkUtils() {
   }
+
 }
