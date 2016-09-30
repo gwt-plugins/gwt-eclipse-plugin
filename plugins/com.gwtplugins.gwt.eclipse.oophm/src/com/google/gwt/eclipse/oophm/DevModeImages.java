62605768852c1a50c06b60cb3d612b19fb90e0d4
branflake2267@gmail.com <branflake2267@gmail.com>
2016-09-29 21:10:43 -0700

    Register image

--- plugins/com.gwtplugins.gwt.eclipse.oophm/src/com/google/gwt/eclipse/oophm/DevModeImages.java
+++ plugins/com.gwtplugins.gwt.eclipse.oophm/src/com/google/gwt/eclipse/oophm/DevModeImages.java
@@ -19,72 +19,52 @@ package com.google.gwt.eclipse.oophm;
  */
 public final class DevModeImages {
 
-  public static final String COLLAPSE_ALL = Activator.PLUGIN_ID
-      + ".images.collapseAll";
+  public static final String COLLAPSE_ALL = Activator.PLUGIN_ID + ".images.collapseAll";
 
-  public static final String CLEAR_LOG = Activator.PLUGIN_ID
-      + ".images.clearLog";
+  public static final String CLEAR_LOG = Activator.PLUGIN_ID + ".images.clearLog";
 
-  public static final String CLEAR_TERMINATED_LAUNCHES = Activator.PLUGIN_ID
-      + ".images.clearTerminatedLaunches";
+  public static final String CLEAR_TERMINATED_LAUNCHES =
+      Activator.PLUGIN_ID + ".images.clearTerminatedLaunches";
 
-  public static final String ERROR_OVERLAY = Activator.PLUGIN_ID
-      + ".images.errorOverlay";
+  public static final String ERROR_OVERLAY = Activator.PLUGIN_ID + ".images.errorOverlay";
 
-  public static final String RELOAD_WEB_SERVER = Activator.PLUGIN_ID
-      + ".images.reloadWebServer";
+  public static final String RELOAD_WEB_SERVER = Activator.PLUGIN_ID + ".images.reloadWebServer";
 
   public static final String GDT_ICON = Activator.PLUGIN_ID + ".images.gdtIcon";
 
-  public static final String GDT_ICON_ERROR = Activator.PLUGIN_ID
-      + ".images.gdtWithError";
+  public static final String GDT_ICON_ERROR = Activator.PLUGIN_ID + ".images.gdtWithError";
 
-  public static final String GDT_ICON_TERMINATED = Activator.PLUGIN_ID
-      + ".images.gdtIconTerminated";
+  public static final String GDT_ICON_TERMINATED =
+      Activator.PLUGIN_ID + ".images.gdtIconTerminated";
 
-  public static final String GDT_ICON_WARNING = Activator.PLUGIN_ID
-      + ".images.gdtWithWarning";
+  public static final String GDT_ICON_WARNING = Activator.PLUGIN_ID + ".images.gdtWithWarning";
 
-  public static final String LOG_ITEM_DEBUG = Activator.PLUGIN_ID
-      + ".images.logItemDebug";
+  public static final String LOG_ITEM_DEBUG = Activator.PLUGIN_ID + ".images.logItemDebug";
 
-  public static final String LOG_ITEM_ERROR = Activator.PLUGIN_ID
-      + ".images.logItemError";
+  public static final String LOG_ITEM_ERROR = Activator.PLUGIN_ID + ".images.logItemError";
 
-  public static final String LOG_ITEM_INFO = Activator.PLUGIN_ID
-      + ".images.logItemInfo";
+  public static final String LOG_ITEM_INFO = Activator.PLUGIN_ID + ".images.logItemInfo";
 
-  public static final String LOG_ITEM_SPAM = Activator.PLUGIN_ID
-      + ".images.logItemSpam";
+  public static final String LOG_ITEM_SPAM = Activator.PLUGIN_ID + ".images.logItemSpam";
 
-  public static final String LOG_ITEM_TRACE = Activator.PLUGIN_ID
-      + ".images.logItemTrace";
+  public static final String LOG_ITEM_TRACE = Activator.PLUGIN_ID + ".images.logItemTrace";
 
-  public static final String LOG_ITEM_WARNING = Activator.PLUGIN_ID
-      + ".images.logItemWarning";
+  public static final String LOG_ITEM_WARNING = Activator.PLUGIN_ID + ".images.logItemWarning";
 
-  public static final String TERMINATE_LAUNCH = Activator.PLUGIN_ID
-      + ".images.terminateLaunch";
+  public static final String TERMINATE_LAUNCH = Activator.PLUGIN_ID + ".images.terminateLaunch";
 
-  public static final String WARNING_OVERLAY = Activator.PLUGIN_ID
-      + ".images.warningOverlay";
+  public static final String WARNING_OVERLAY = Activator.PLUGIN_ID + ".images.warningOverlay";
 
-  public static final String WEB_BROWSER = Activator.PLUGIN_ID
-      + ".images.webBrowser";
+  public static final String WEB_BROWSER = Activator.PLUGIN_ID + ".images.webBrowser";
 
-  public static final String WEB_BROWSER_ERROR = Activator.PLUGIN_ID
-      + ".images.webBrowserWithError";
+  public static final String WEB_BROWSER_ERROR =
+      Activator.PLUGIN_ID + ".images.webBrowserWithError";
 
-  public static final String WEB_BROWSER_TERMINATED = Activator.PLUGIN_ID
-      + ".images.webBrowserTerminated";
+  public static final String WEB_BROWSER_TERMINATED =
+      Activator.PLUGIN_ID + ".images.webBrowserTerminated";
 
-  public static final String WEB_BROWSER_WARNING = Activator.PLUGIN_ID
-      + ".images.webBrowserWithWarning";
+  public static final String WEB_BROWSER_WARNING =
+      Activator.PLUGIN_ID + ".images.webBrowserWithWarning";
 
-  public static final String SPEED_TRACER_ICON = Activator.PLUGIN_ID
-      + ".images.speedTracerIcon";
-
-  public static final String SPEED_TRACER_ICON_TERMINATED = Activator.PLUGIN_ID
-      + ".images.speedTracerIconTerminated";
-
+  public static final String GWT_OOPHM = Activator.PLUGIN_ID + ".images.gwtOophm";
 }
