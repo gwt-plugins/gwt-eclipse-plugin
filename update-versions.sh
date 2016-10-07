9ca45a9aad5d45fe66fe46004243d2b7af72f520
branflake2267@gmail.com <branflake2267@gmail.com>
2016-10-07 12:29:52 -0700

    Add version up script.

---
+++ update-versions.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# https://eclipse.org/tycho/sitedocs/tycho-release/tycho-versions-plugin/set-version-mojo.html
+
+mvn org.eclipse.tycho:tycho-versions-plugin:set-version -DnewVersion=3.0.0-SNAPSHOT
+
